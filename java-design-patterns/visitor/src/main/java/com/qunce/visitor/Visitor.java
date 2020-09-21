95dc816e200f0811a54c75ea5897089f88c090cd
soulgoast <qq928919122>
2020-09-21 16:34:54 +0800

    访问者模式

---
+++ java-design-patterns/visitor/src/main/java/com/qunce/visitor/Visitor.java
@@ -0,0 +1,15 @@
+package com.qunce.visitor;
+
+/**
+ * @ClassName Visitor
+ * @Description TODO
+ * @Author hu zhongxi
+ * @email m18967896507_1@163.com
+ * @Date 2020/9/21 15:49
+ * @ModifyDate 2020/9/21 15:49
+ * @Version 1.0
+ */
+public class Visitor {
+
+
+}
